7b620f67123d88f3919ac88a944cded93e98d5e5
nieltabar <73207731+nieltabar@users.noreply.github.com>
2021-11-17 18:14:20 +0800

    changes

--- server.js
+++ server.js
@@ -2,26 +2,20 @@
 var express = require('express'); // requre the express framework
 var app = express();
 var fs = require('fs'); //require file system object
-const mysql = require('mysql');
 
-
+app.get('/', function(req, res){
+        res.send("Hello World"); 
+}
 // Endpoint to Get a list of users
-app.get('/getUsers',(req, res) => {
-   const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'metroevent'
-})
-
-db.query("SELECT * FROM user",(err, rows, fields)=>{
-    console.log("Sucess");
-    res.json(rows)
-})
+app.get('/getUsers', function(req, res){
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+        console.log(data);
+        res.send(data); // you can also use res.send()
+    })
 })
 
 // Create a server to listen at port 8080
-var server = app.listen(process.env.port || 8080, function(){
+var server = app.listen(process.env.PORT 8080, function(){
     var host = server.address().address
     var port = server.address().port
     console.log("REST API demo app listening at http://%s:%s", host, port)
